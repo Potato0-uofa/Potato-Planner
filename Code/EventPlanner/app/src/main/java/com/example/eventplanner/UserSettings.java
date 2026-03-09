2917ca53201df5cec2170300a0e792c4d99cd49d
Eypan <eypan@ualberta.ca>
2026-03-09 09:24:17 -0600

    Created UserSettings Java class and Settings UI

---
+++ Code/EventPlanner/app/src/main/java/com/example/eventplanner/UserSettings.java
@@ -0,0 +1,74 @@
+package com.example.eventplanner;
+
+public class UserSettings {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String country;
+    private boolean notificationsFromOrganizers;
+    private boolean notificationsFromAdmins;
+    private boolean appUpdates;
+
+    public UserSettings() { // Required for Firestore
+    }
+
+    // Constructor for UserSettings
+    public UserSettings(String name, String username, String email, String phoneNumber, String
+                        country, boolean notificationsFromOrganizers,
+                        boolean notificationsFromAdmins, boolean appUpdates) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.country = country;
+        this.notificationsFromOrganizers = notificationsFromOrganizers;
+        this.notificationsFromAdmins = notificationsFromAdmins;
+        this.appUpdates = appUpdates;
+    }
+
+    // Setting up Getters
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public boolean isNotificationsFromOrganizers() {
+        return notificationsFromOrganizers;
+    }
+    public boolean isNotificationsFromAdmins() {
+        return notificationsFromAdmins;
+    }
+    public boolean isAppUpdates() {
+        return appUpdates;
+    }
+
+    // Setting up Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public void setNotificationsFromOrganizers(boolean v) {
+        this.notificationsFromOrganizers = v;
+    }
+    public void setNotificationsFromAdmins(boolean v) {
+        this.notificationsFromAdmins = v;
+    }
+    public void setAppUpdates(boolean v) {
+        this.appUpdates = v;
+    }
+
+}
