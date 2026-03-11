b251bdf5b7add3a1eeecd1409092b6eb36c12e69
IvanBaldo67 <ibaldo@ualberta.ca>
2026-03-10 23:09:16 -0600

    Implemented methods fro Events that pertain to its specific waiting list as well as implements java doc comments for waitingList class and Events Class

--- Code/EventPlanner/app/src/main/java/com/example/eventplanner/Events.java
+++ Code/EventPlanner/app/src/main/java/com/example/eventplanner/Events.java
@@ -1,30 +1,133 @@
 package com.example.eventplanner;
 
+import java.util.List;
+
+/**
+ * Represents an event in the event planner application.
+ * Each event has a name, date, description, location, and a waiting list of entrants.
+ */
+
 public class Events {
     //attributes of the events
     private String name;
     private String date;
     private String description;
     private String location;
+    private WaitingList waitingList;
 
-    //constructor
+    /**
+     * Default constructor required for Firestore.
+     * Initializes an empty waiting list.
+     */
+    public Events(){
+        this.waitingList = new WaitingList();
+    }
+
+    /**
+     * Constructs an Events object with the specified details.
+     *
+     * @param name        the name of the event
+     * @param date        the date of the event
+     * @param description a brief description of the event
+     * @param location    the location where the event will be held
+     */
     public Events(String name, String date, String description, String location) {
         this.name = name;
         this.date = date;
         this.description = description;
         this.location = location;
+        this.waitingList = new WaitingList();
     }
 
-    //getters
+    /**
+     * Adds an entrant to the waiting list for this event.
+     * Does nothing if entrant is already on the list.
+     *
+     * @param entrant the entrant to add to the waiting list
+     */
+    public void addToWaitingList(Entrant entrant) {
+        if (!waitingList.hasEntrant(entrant)) {
+            waitingList.addEntrant(entrant);
+        }
+    }
+
+    /**
+     * Removes an entrant from the waiting list for this event.
+     * Does nothing if the entrant is not on the waiting list.
+     *
+     * @param entrant the entrant to remove from the waiting list
+     */
+    public void removeFromWaitingList(Entrant entrant) {
+        if (waitingList.hasEntrant(entrant)){
+            waitingList.deleteEntrant(entrant);
+        }
+    }
+
+    /**
+     * Checks whether a given entrant is on the waiting list for this event.
+     *
+     * @param entrant the entrant to check
+     * @return true if the entrant is on the waiting list, false otherwise
+     */
+    public boolean isOnWaitingList(Entrant entrant) {
+        return waitingList.hasEntrant(entrant);
+    }
+
+    /**
+     * Returns the waiting list for this event.
+     *
+     * @return the WaitingList object associated with this event
+     */
+    public WaitingList getWaitingList() {
+        return waitingList;
+    }
+
+    /**
+     * Returns the name of the event.
+     * @return event name
+     */
     public String getName() { return name; }
+
+    /**
+     * Returns the date of the event.
+     * @return event date
+     */
     public String getDate() { return date; }
+
+    /**
+     * Returns the event description.
+     * @return event description
+     */
     public String getDescription() { return description; }
-    public String getLoction() {return location; }
+
+    /**
+     * Returns the location of the event.
+     * @return event location
+     */
+    public String getLocation() {return location; }
 
 
-    //setters
+    /**
+     * Sets the name of the event.
+     * @param name the event name
+     */
     public void setName(String name) { this.name = name; }
+
+    /**
+     * Sets the date of the event.
+     * @param date the date of the event
+     */
     public void setDate(String date) { this.date = date; }
+
+    /**
+     * Sets the description of the event.
+     * @param description the event description
+     */
     public void setDescription(String description) { this.description = description; }
+
+    /**
+     * Sets the location of the event
+     * @param location the event's location
+     */
     public void setLocation(String location) {this.location = location;}
 }
--- Code/EventPlanner/app/src/main/java/com/example/eventplanner/WaitingList.java
+++ Code/EventPlanner/app/src/main/java/com/example/eventplanner/WaitingList.java
@@ -1,24 +1,45 @@
 package com.example.eventplanner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WaitingList extends CustomLogs{
-    private List<Entrant> entrants;
+    private List<Entrant> waitingList;
+    public WaitingList(){
+        this.entrants = new ArrayList<>();
+    }
 
     public void addEntrant(Entrant entrant) {
-        addItem(entrant);
+        /**
+         * Adds an entrant to the waiting list.
+         * @param entrant Entrant to be added
+         */
+        waitingList.add(entrant);
     }
 
     public boolean hasEntrant(Entrant entrant) {
-        return hasItem(entrant);
+        /**
+         * Checks to see if the entrant is in the list or not.
+         * @param entrant Entrant that needs to be checked
+         * @return True if entrant is in the list and false if not
+         */
+        return waitingList.contains(entrant);
     }
 
     public void deleteEntrant(Entrant entrant) {
-        deleteItem(entrant);
+        /**
+         * Remove entrant from waiting list.
+         * @param entrant Entrant to be removed
+         */
+        waitingList.remove(entrant);
     }
 
-    public int countEntrant() {
+    public int countEntrants() {
+        /**
+         * Checks to see how many entrants are in the waiting list.
+         * @return int value that represents how many entrants there are
+         */
         return countItems();
     }
 }
-}
+
